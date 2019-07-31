23ad766957cb64ccfb831a65eaa3908a49735263
Juliana JEAN-MARIE <JJean-marie@oui.sncf>
2019-07-31 16:07:53 +0200

    formatting

--- api/app.js
+++ api/app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
-const bodyParser          = require("body-parser");
-const cookieParser        = require("cookie-parser");
-const express             = require("express");
-const session             = require('express-session');
-const connect             = require("./config/db");
-const passport            = require("./config/authentication");
-const logger              = require("morgan");
-const path                = require("path");
-var serveStatic           = require("serve-static");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const session = require("express-session");
+const connect = require("./config/db");
+const passport = require("./config/authentication");
+const logger = require("morgan");
+const path = require("path");
+var serveStatic = require("serve-static");
 
 // mongoose connect
 connect();
@@ -27,19 +27,19 @@ app.use(serveStatic(path.join(__dirname, "../frontend/public")));
 // app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 const index = require("./routes/index");
-const authRoutes = require('./routes/authentication');
-app.use('/auth', authRoutes);
+const authRoutes = require("./routes/authentication");
+app.use("/auth", authRoutes);
 app.use("/api", index);
 
 // error handler
 app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log(err, res, req)
-    // send the error
-    res.status(err.status || 500);
-    res.json({message: err.message});
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.log(err, res, req);
+  // send the error
+  res.status(err.status || 500);
+  res.json({ message: err.message });
 });
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const router  = express.Router();
-const Barter = require('../models/barter');
-const uploadCloud = require('../config/cloudinary.js');
+const express = require("express");
+const router = express.Router();
+const Barter = require("../models/barter");
+const uploadCloud = require("../config/cloudinary.js");
 
 /* GET home page */
 // router.get('/', (req, res, next) => {
 //   res.render('index');
 // });
 
-router.route('/trocs').get((req, res) => {
-  res.json({label:"hello"});
+router.route("/trocs").get((req, res) => {
+  res.json({ label: "hello" });
 });
 
-
 // Add new barter
-router.post('/trocs',  uploadCloud.single('image'), (req, res) => {
+router.post("/trocs", uploadCloud.single("image"), (req, res) => {
   const { title, description, condition, category } = req.body;
   var new_barter = new Barter({
     title,
@@ -22,18 +21,17 @@ router.post('/trocs',  uploadCloud.single('image'), (req, res) => {
     description,
     condition,
     imageUrl: req.file.url
-  })
+  });
 
-  new_barter.save(function (error) {
+  new_barter.save(function(error) {
     if (error) {
-      console.log(error)
+      console.log(error);
     }
     res.send({
       success: true,
-      message: 'Post saved successfully!'
-    })
-  })
-})
-
+      message: "Post saved successfully!"
+    });
+  });
+});
 
 module.exports = router;
